3329222dc1d81f2b8ab3710789ce88de02c773c6
zhujiayao1 <145834611+zhujiayao1@users.noreply.github.com>
2024-10-10 17:12:30 +0800

    post donation

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,15 @@
+/*
+ * @Descripttion: Web-A3
+ * @Author: Zhujiayao & Luchenchen
+ */
 var express = require('express');
 var router = express.Router();
 var pool    = require('../db/crowdfunding_db');
-//
+
+router.use(express.json()); // 用于解析JSON格式的请求体
+// 使用中间件解析URL编码的请求体
+router.use(express.urlencoded({ extended: true }));
+
 router.get('/indexbox',function(req,res,next){
 	pool.getConnection(function(err,connection){
 		if (err) {
@@ -31,8 +39,9 @@ router.get('/Search',function(req,res,next){
 		if (err) {
 			res.send('Connection error')
 		}
+		// SELECT f.FUNDRAISER_ID, f.ORGANIZER, f.CAPTION, f.TARGET_FUNDING, f.CURRENT_FUNDING, f.CITY,f.CONTENT
 		let query = `
-		SELECT f.FUNDRAISER_ID, f.ORGANIZER, f.CAPTION, f.TARGET_FUNDING, f.CURRENT_FUNDING, f.CITY,f.CONTENT
+		SELECT *
 		FROM FUNDRAISER f
 		LEFT JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID
 		WHERE 1=1
@@ -90,6 +99,42 @@ router.get('/queryById', function (req, res, next) {
 			connection.release();
 		})
 	})
-});
+})
+
+router.post('/donation', function (req, res, next) {
+	pool.getConnection(function(err,connection){
+		if (err) {
+			res.send('Connection error')
+		}
+		const date = req.query.date
+		const amount = req.query.amount
+		const giver = req.query.giver
+		const fundraiserId = req.query.fundraiserId
+
+		console.log(date, amount, giver, fundraiserId);//测试拿到参数
+		if (!date || !amount || !giver || !fundraiserId) {
+            // 如果数据不完整，返回400错误
+            return res.status(400).send('缺少必要的捐赠字段');
+        }
+		// 准备插入数据库的SQL语句
+		const query = 'INSERT INTO DONATION (DATE, AMOUNT, GIVER, FUNDRAISER_ID) VALUES (?, ?, ?, ?)';
+
+		connection.query(query, [date, amount, giver, fundraiserId],function(err,rows){
+			if (err) {
+				console.log(err)
+				res.send('Query failure')
+			}
+			res.send("donation insert success")
+			connection.release();
+		})
+	})
+})
+
+
+
+
+;
+
+
 
 module.exports = router;

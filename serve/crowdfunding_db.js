19fac4978ee449eac8392ece61e07abdbd69b5d3
zhujiayao1 <145834611+zhujiayao1@users.noreply.github.com>
2024-10-14 15:39:37 +0800

    update api

--- serve/crowdfunding_db.js
+++ serve/crowdfunding_db.js
@@ -1,25 +1,16 @@
-const express = require('express');
 const mysql = require('mysql');
-const port = 3000;
-const app = express();
 
+// Import our db details
+var db = require("./db_details.js");
 
-// Create a database connection.
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '123456',
-  port: '3307',
-  database: 'crowdfunding_db'
-});
-
-// Establish a database connection.
-connection.connect(err => {
-  if (err) throw err;
-  console.log('Connected to the database.');
-});
-
-// To start an Express server
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+// Create db Connection
+module.exports = {
+    getConnection: ()=>{
+        return mysql.createConnection({
+            host: db.host,
+            user: db.user,
+            password: db.password,
+            database: db.database 
+        });
+    }
+};
---
+++ serve/db_details.js
@@ -0,0 +1,7 @@
+module.exports =
+{
+    host: "localhost",
+    user: "jzhu31_jzhu",
+    password: "x-d%&b+VhH+R",
+    database: "jzhu31_crowdfunding_db"
+};
